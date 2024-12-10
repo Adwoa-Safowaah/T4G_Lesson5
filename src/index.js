29f05010ea0573ef91b03719ee5aa4e0e0c71589
Adwoa-Safowaah <adwoaadysne@gmail.com>
2024-12-10 22:42:44 +0000

    I updated all of my files.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
+//function changeButtonStyle() {
+    //button.style.backgroundColor="blue"; 
+    ///button.style.color="white";
 
+//}
 
-function changeButtonStyle() {
-    button.style.backgroundColor="blue"; 
-    button.style.color="white";
+// DOM
+document.addEventListener("DOMContentLoaded", () =>{
+    let openButton = document.querySelector("#open-button");
+    let closeButton = document.querySelector("#close-button");
+    let imageContainer = document.querySelector("#image-container");
 
-}
+// Show image and close button, hide open button
+openButton.addEventListener("click", () => {
+    imageContainer.style.display = "block";
+    closeButton.style.display = "inline-block";
+    openButton.style.display = "none";
+});
 
-
-let button = document.querySelector("button");
-button.addEventListener("click", changeButtonStyle);
-
-//this section open and closes and image
-let open = document.querySelector("open")
-open1.addEventListener("click", showitems);
+// Hide image and close button, show open button
+closeButton.addEventListener("click", () => {
+    imageContainer.style.display = "none";
+    closeButton.style.display = "none";
+    openButton.style.display = "inline-block";
+});
+});
